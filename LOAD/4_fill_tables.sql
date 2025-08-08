e0420b717617528eb61737e0d68b3c7950d38f17
M-R7 <riviere.morgane.7@gmail.com>
2025-08-08 13:34:54 +0200

    add load files files_csv

--- LOAD/4_fill_tables.sql
+++ LOAD/4_fill_tables.sql
@@ -16,23 +16,35 @@ SELECT
   EXTRACT(MONTH FROM date_full) AS month,
   EXTRACT(DAY FROM date_full) AS day,
   EXTRACT(YEAR FROM date_full) AS year,
-  EXTRACT(QUARTER FROM date_full) AS quarter_num,
+  EXTRACT(QUARTER FROM date_full) AS quarter,
   -- Libellés lisibles
-  TO_CHAR(MONTHNAME(date_full)) AS month_name,
-  TO_CHAR(DAYNAME(date_full)) AS day_name,
+  TO_CHAR(date_full, 'Month') AS month_name,
+  TO_CHAR(date_full, 'Day') AS day_name,
   -- Mois + année
   TO_NUMBER(TO_CHAR(date_full, 'YYYYMM')) AS month_year_id,
   TO_CHAR(date_full, 'YYYY-MM') AS month_year,
-  -- Jour de la semaine (1=lundi ... 7=dimanche)
-  DATE_PART(DAYOFWEEK_ISO, date_full) AS weekday_number,
-  -- Jour ouvré ou non
+  -- Jour de la semaine
+  DATE_PART(DAYOFWEEK, date_full) AS weekday_number,       -- 1 = dimanche
+  TO_CHAR(date_full, 'DY') AS weekday,                      -- ex: MON, TUE
   CASE
-      WHEN DATE_PART(DAYOFWEEK_ISO, date_full) IN (6, 7) THEN FALSE
-      ELSE TRUE
+    WHEN DATE_PART(DAYOFWEEK, date_full) IN (1, 7) THEN FALSE
+    ELSE TRUE
   END AS is_weekday,
   -- Quarter
-  'Q' || EXTRACT(QUARTER FROM date_full) AS quarter_label,
+  TO_CHAR(date_full, '"Q"Q') AS quarter_label,
   -- Quarter + année
   TO_NUMBER(TO_CHAR(date_full, 'YYYY') || EXTRACT(QUARTER FROM date_full)) AS quarter_year_id,
   TO_CHAR(date_full, 'YYYY') || '-Q' || EXTRACT(QUARTER FROM date_full) AS quarter_year
-FROM dates;
+FROM dates
+
+--- Remplissage de la table FIAT_INFO
+-- Utilisation du datawarehouse QUERY
+USE WAREHOUSE QUERY;
+-- Utilisation de la database RAW_DATA et du schéma calendar
+USE DATABASE RAW_DATA;
+USE SCHEMA GITREPO;
+
+-- Remplissage de la table depuis le fichier dans le stage
+COPY INTO FIAT_INFO
+FROM @MY_STAGE/table_fiat.csv
+FILE_FORMAT = CLASSIC_CSV;
