2bad63751a8f57f74defe9a41e26b8a72335b52f
M-R7 <riviere.morgane.7@gmail.com>
2025-08-09 09:08:37 +0200

    modification calendar table request

--- LOAD/4_fill_tables.sql
+++ LOAD/4_fill_tables.sql
@@ -7,7 +7,7 @@ USE SCHEMA calendar;
 
 CREATE OR REPLACE TABLE calendar AS
 WITH dates AS (
-  SELECT DATEADD(day, seq4(), '2020-01-01') AS date_full
+  SELECT DATEADD(day, seq4(), DATE '2020-01-01') AS date_full
   FROM TABLE(GENERATOR(ROWCOUNT => 365 * 10))  -- 10 ans
 )
 SELECT
@@ -18,24 +18,23 @@ SELECT
   EXTRACT(YEAR FROM date_full) AS year,
   EXTRACT(QUARTER FROM date_full) AS quarter,
   -- Libellés lisibles
-  TO_CHAR(date_full, 'Month') AS month_name,
-  TO_CHAR(date_full, 'Day') AS day_name,
+  TO_CHAR(date_full, 'Mon') AS month_name,
+  TO_CHAR(date_full, 'DY') AS day_name,
   -- Mois + année
   TO_NUMBER(TO_CHAR(date_full, 'YYYYMM')) AS month_year_id,
   TO_CHAR(date_full, 'YYYY-MM') AS month_year,
   -- Jour de la semaine
-  DATE_PART(DAYOFWEEK, date_full) AS weekday_number,       -- 1 = dimanche
-  TO_CHAR(date_full, 'DY') AS weekday,                      -- ex: MON, TUE
+  DATE_PART(DAYOFWEEK_ISO, date_full) AS weekday_number,       -- 1 = dimanche                     -- ex: MON, TUE
   CASE
-    WHEN DATE_PART(DAYOFWEEK, date_full) IN (1, 7) THEN FALSE
+    WHEN DATE_PART(DAYOFWEEK_ISO, date_full) IN (6, 7) THEN FALSE
     ELSE TRUE
   END AS is_weekday,
   -- Quarter
-  TO_CHAR(date_full, '"Q"Q') AS quarter_label,
+  'Q' || EXTRACT(QUARTER FROM date_full) AS quarter_label,
   -- Quarter + année
   TO_NUMBER(TO_CHAR(date_full, 'YYYY') || EXTRACT(QUARTER FROM date_full)) AS quarter_year_id,
   TO_CHAR(date_full, 'YYYY') || '-Q' || EXTRACT(QUARTER FROM date_full) AS quarter_year
-FROM dates
+FROM dates;
 
 --- Remplissage de la table FIAT_INFO
 -- Utilisation du datawarehouse QUERY
