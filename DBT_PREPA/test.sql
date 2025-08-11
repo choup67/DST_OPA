91e486765869675631628c2cd6708a9091f26ecc
choup67 <choup67l@gmail.com>
2025-08-11 14:35:40 +0200

    staging asset_info and exchange_info + schema.yml

--- DBT_PREPA/test.sql
+++ DBT_PREPA/test.sql
@@ -4,3 +4,17 @@ USE SCHEMA STAR_SCHEMA;
 SELECT *
 FROM DIMCUSTOMER
 LIMIT 10;
+
+
+use database DBT;
+USE SCHEMA STAGING;
+select coingecko_id, asset, count(*) as n
+from STG_coingecko__asset_info
+where asset is not null
+group by 1
+having count(*) > 1
+order by n desc;
+
+select *
+from STG_coingecko__asset_info
+where coingecko_id ilike '%binance%';
