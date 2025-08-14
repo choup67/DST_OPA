3c1cf08b94269cfd157317f5b720c9ccddee2965
choup67 <choup67l@gmail.com>
2025-08-14 10:25:27 +0200

    dbt tests ok + schemas + fix calendar issue

--- DBT_PREPA/test.sql
+++ DBT_PREPA/test.sql
@@ -7,7 +7,7 @@ LIMIT 10;
 
 
 use database DBT;
-USE SCHEMA STAGING;
+USE SCHEMA intermediate;
 select coingecko_id, asset, count(*) as n
 from STG_coingecko__asset_info
 where asset is not null
@@ -17,3 +17,22 @@ order by n desc;
 
 select *
 from stg_gitrepo__fiat_info;
+
+select *
+from int_binance__kline
+where volume < 0 or quote_asset_volume < 0
+order by open_time desc
+limit 50;
+
+select distinct date_jour, date_full
+from fact_klines
+left join dim_calendar
+on fact_klines.date_jour = dim_calendar.date_full
+where dim_calendar.year is  null
+order by date_jour asc;
+
+USE SCHEMA mart;
+
+select distinct date_full
+from dim_calendar
+where date_full = '2019-09-25';
